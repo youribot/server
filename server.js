8c519a5b4455ef792c19a61473aa0f874868712c
niksmac <hello@nikhilmohan.in>
2017-01-25 12:05:20 +0530

    Configured IPFS

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 
 const Hapi = require('hapi')
 var _ = require('lodash')
-require('locus');
+require('locus')
+var ipfsAPI = require('ipfs-api')
+var ipfs = ipfsAPI({host: '192.168.27.101', port: '5001', protocol: 'http'})
+
+// eval(locus)
 
 var server = new Hapi.Server()
 server.connection({
