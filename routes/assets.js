7f399071004ccedf0d679f4c50957a5077abd3b7
niksmac <hello@nikhilmohan.in>
2017-01-25 12:37:38 +0530

    Added endpoint to upload file

--- routes/assets.js
+++ routes/assets.js
@@ -1,9 +1,40 @@
+var fs = require('fs')
+
 var routes = [
   {
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      return reply('Hello world')
+    method: 'POST',
+    path: '/asset/upload',
+    config: {
+
+      payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data'
+      },
+
+      handler: function (request, reply) {
+        console.log('here')
+        var data = request.payload
+        if (data.file) {
+          var name = data.file.hapi.filename
+          var path = process.cwd() + '/uploads/' + name
+          var file = fs.createWriteStream(path)
+
+          file.on('error', function (err) {
+            console.error(err)
+          })
+
+          data.file.pipe(file)
+
+          data.file.on('end', function (err) {
+            var ret = {
+              filename: data.file.hapi.filename,
+              headers: data.file.hapi.headers
+            }
+            reply(JSON.stringify(ret))
+          })
+        }
+      }
     }
   }
 ]
